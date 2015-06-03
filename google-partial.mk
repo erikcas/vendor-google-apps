8a51e31f05d9cf75b5ddd02fce8091479f9c9e9b
Erik Castricum <erikcas1972@gmail.com>
2015-06-03 19:51:34 +0200

    initial

---
+++ google-partial.mk
@@ -0,0 +1,13 @@
+PRODUCT_PACKAGES += \
+	FaceLock \
+	GoogleContactsSyncAdapter \
+	GoogleBackupTransport \
+	GoogleFeedback \
+	GoogleLoginService \
+	GoogleOneTimeInitializer \
+	GooglePartnerSetup \
+	GoogleServicesFramework \
+	Phonesky \
+	PrebuiltGmsCore \
+	SetupWizard \
+	Velvet
---
+++ vendor-google.mk
@@ -0,0 +1,2 @@
+$(call inherit-product, vendor/apps/google-partial.mk)
+$(call inherit-product-if-exists, vendor/apps/vendor-google-apps.mk)
